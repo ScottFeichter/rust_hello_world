16f256237fb178bf56df8b1a24e168e442a409f0
Scott Feichter <scottfeichter@gmail.com>
2024-09-29 16:22:09 -0700

    misc (msg via sh alias)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// cargo new - create a new project
+// cargo build - runs compile and makes executable
+// cargo check - runs compile but doesn't make executable
+// cargo run - does the compile/build if necessary and then run
+
+// cargo build --release : compiles with opitimizations for prod
